870eefcad19e0cac6032b798ff222b8ea02af158
MOHANESH <smmohanesh3@gmail.com>
2025-01-22 10:27:32 +0530

    prime number completed

--- Task 1/amstrong_number.py
+++ Task 1/amstrong_number.py
@@ -3,7 +3,7 @@
  Objective  : Check if a number equals the sum of its digits raised to the power of the number of digits.
  Input      : An integer nnn.
  Output     : True or False.
- Example    : 153 is an Armstrong number because 13+33=53+1531^3 + 5^3 + 3^3 = 15313+53+33=153.
+ Example    : 153 is an Armstrong number because 13+33=53+153 1^3 + 5^3 + 3^3 = 153 13+53+33=153.
  Hint       : Convert to a string to calculate the length and power."""
 def amstrong_number(x):
     len_of_num = len(str(x))
---
+++ Task 2/primenumbers.py
@@ -0,0 +1,19 @@
+# this is task 2 question 9
+""" 9. Prime Number
+ Objective  : Determine if a number is prime.
+ Input      : A single integer nnn.
+ Output     : True if prime, otherwise False.
+ Hint       : A prime number has no divisors other than 1 and itself; check divisors up to n\sqrt{n}n  """
+def prime_number(number):
+    if number > 1:
+      for i in range(2,(number//2)+1):
+        if (number % i)== 0:
+          return False
+        else:
+          return "the number is prime number"+True
+    else :
+      return False
+    
+number = int(input("enter the number"))
+print(prime_number(number))
+        
