9aba605084051b84bbedefcc9bf0b6645aa5c0bf
MOHANESH <smmohanesh3@gmail.com>
2025-01-22 10:34:34 +0530

    prime number modified

--- Task 2/primenumbers.py
+++ Task 2/primenumbers.py
@@ -5,14 +5,12 @@
  Output     : True if prime, otherwise False.
  Hint       : A prime number has no divisors other than 1 and itself; check divisors up to n\sqrt{n}n  """
 def prime_number(number):
-    if number > 1:
-      for i in range(2,(number//2)+1):
-        if (number % i)== 0:
-          return False
-        else:
-          return "the number is prime number"+True
-    else :
+    if number <= 1:
       return False
+    for i in range(2,(number//2)+1):
+      if number%i == 0 :
+         return False
+    return True
     
 number = int(input("enter the number"))
 print(prime_number(number))
